8b3edda146c302f5759212e383ed5dc5ee09988d
Cameron <cameron.kerestus@gmail.com>
2024-03-06 02:34:42 -0500

    Added api functionality for access to tasks and removal/addition of tasks

--- intellitask/apps/api/urls.py
+++ intellitask/apps/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/tasks/', views.task_list, name='task_list'),
+    path('api/tasks/', views.api_task_list, name='api_task_list'),
+    path('api/tasks/<int:pk>/', views.task_detail, name='task_detail'),
 ]
--- intellitask/apps/api/views.py
+++ intellitask/apps/api/views.py
@@ -6,7 +6,7 @@ from apps.core.models import Task
 from .serializers import TaskSerializer
 
 @api_view(['GET', 'POST'])
-def task_list(request):
+def api_task_list(request):
     if request.method == 'GET':
         if request.user.is_authenticated:  # Check if user is authenticated
             tasks = Task.objects.filter(user=request.user)  # Filter by logged-in user
@@ -24,3 +24,28 @@ def task_list(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"detail": "Authentication credentials were not provided."}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def task_detail(request, pk):
+    """
+    Retrieve, update or delete a specific task.
+    """
+    try:
+        task = Task.objects.get(pk=pk, user=request.user) # Enforce ownership
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save() # Update existing task
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
